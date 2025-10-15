b3d30539e35d0007e5655050136ff85f8c2f934b
Laxman Maharjan <hotmale_luksh@hotmail.com>
2025-10-15 18:05:54 +0545

    Admin Portal Forgot Password

--- ai_solution/urls.py
+++ ai_solution/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from dashboard.views import CustomAdminLoginView
 admin.site.login = CustomAdminLoginView.as_view()
 urlpatterns = [
+    # Admin password reset views (must be BEFORE admin/ to avoid catch-all)
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
